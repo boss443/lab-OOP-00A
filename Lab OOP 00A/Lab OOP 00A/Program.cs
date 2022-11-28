111d9834787a0e5b8c02f850b2d0fa6822a6a64d
Winyu64 <thanachok.s@kkumail.com>
2022-11-28 15:36:37 +0700

    Add name

---
+++ Lab OOP 00A/Lab OOP 00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// print 
+Console.WriteLine("Thanachok Suwan");
+
+Console.WriteLine("653450257-7");
+
+Console.WriteLine("thanachok.s@kkkumail.com");
